2a917fb96e5f1ed1ff0e0a8f7e6d65f97944a534
Thais Leal <58890658+thaispll@users.noreply.github.com>
2020-08-05 18:07:25 -0300

    Create stringmessage.cs

---
+++ stringmessage.cs
@@ -0,0 +1,9 @@
+string message = "The quick brown fox jumps over the lazy dog.";
+bool result = message.Contains("dog");
+Console.WriteLine(result);
+
+if (message.Contains("fox"))
+{
+    Console.WriteLine("What does the fox say?");
+}
+
